c881a6a16a71df85a27668780c9b0382170be6cc
vasconcelos1898 <55930398+vasconcelos-gabriel@users.noreply.github.com>
2022-10-14 17:57:06 -0300

    server changes

---
+++ eSports/server/README.md
@@ -0,0 +1,26 @@
+# Back-end
+
+## Entidades
+
+### Game -
+
+id
+title
+bannerUrl
+
+### Ad - 
+id
+gameId
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+## Casos de uso
+ - Listagem de games com contagem de anúncios
+ - Ciranção de novo anúncio
+ - Listagem de anúncio por game
+ - Buscar dicord pelo ID do anúncio
--- eSports/server/src/server.ts
+++ eSports/server/src/server.ts
@@ -2,14 +2,30 @@ import express from 'express'
 
 const app = express()
 
-// minhaapi.com/ads
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+
   return response.json([
     { id: 1, name: 'Anuncio 1' },
     { id: 2, name: 'Anuncio 2' },
     { id: 3, name: 'Anuncio 3' },
-    { id: 4, name: 'Anuncio 4' }
+    { id: 4, name: 'Anuncio 4' },
+    { id: 5, name: 'Anuncio 5' },
+    { id: 6, name: 'Anuncio 6' }
   ])
 })
 
+app.get('/games/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+
+  return response.json([])
+})
 app.listen(3333)
